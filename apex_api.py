0acf88837098e9195d176d66d4d66939a3627636
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2021-03-11 09:18:16 -0800

    Verify system

---
+++ apex_api.py
@@ -0,0 +1,16 @@
+import requests
+import config
+
+APEX_URL = f"https://public-api.tracker.gg/v2/apex/standard/profile/"
+
+
+def get_apex_rank(name):
+    platform = "origin"
+    try:
+        request = requests.get(f"{APEX_URL.lower()}{platform.lower()}/{name.lower()}",
+                               headers={"TRN-Api-Key": config.APEX_KEY})
+        rang = request.json()
+        print(rang)
+        return str(rang['data']["segments"][0]["stats"]['rankScore']["metadata"]['rankName'])
+    except:
+        return -1  # "Wrong name dude, you have to type your Origin name or tracker.gg is broken."
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import requests
+
 from discord.utils import get
 import datetime
 
@@ -9,18 +9,13 @@ from sqlalchemy.orm import sessionmaker, query
 
 import config
 from models import DiscordUser, MediaPost
+from apex_api import get_apex_rank
 
 intents = discord.Intents.default()
 intents.members = True
 
 client = commands.Bot(command_prefix='!', intents=intents)
-TOKEN = "Nzg2MDI5MzEyNzg4NzkxMzQ2.X9Ac1w.TRZVEdtnSA4NyTh0Yyo-pXmi1a4"
 
-# Discord
-CLIENT_ID = 786029312788791346
-PUBLIC_KEY = '5abb7a5d430e5158b68b6b217f4ac1bc0e5065c0a3093ccdad53ede613869390'
-
-CLIENT_SECRET = "nblp34-XOk8gO4vhOs4e4fxQYShoKYIi"
 APEX_ROLES = ['Bronze 1', 'Bronze 2', 'Bronze 3', 'Bronze 4', 'Silver 1', 'Silver 2', 'Silver 3', 'Silver 4', "Gold 1",
               "Gold 2", "Gold 3", "Gold 4"]
 
@@ -31,28 +26,74 @@ engine_config = f'{config.DB_DATABASE_TYPE}://{config.DB_USER}:{config.DB_PASSWO
 engine = create_engine(engine_config, echo=True)
 Session = sessionmaker(bind=engine)
 
-"""name  - epiz_27963539,  pass -o8vSQ3DjMGQ, host name - sql105.epizy.com, data_name - epiz_27963539_tpgdatabace,"""
-"https://discord.com/api/oauth2/authorize?client_id=786029312788791346&permissions=2014313665&scope=bot"
-# Apex
-APEX_KEY = "cc301c53-f007-455e-b3da-0fdd828c6523"
-APEX_URL = f"https://public-api.tracker.gg/v2/apex/standard/profile/"
-
-
-def get_apex_rank(name):
-    platform = "origin"
-    try:
-        request = requests.get(f"{APEX_URL.lower()}{platform.lower()}/{name.lower()}",
-                               headers={"TRN-Api-Key": APEX_KEY})
-        rang = request.json()
-        print(rang)
-        return str(rang['data']["segments"][0]["stats"]['rankScore']["metadata"]['rankName'])
-    except:
-        return -1  # "Wrong name dude, you have to type your Origin name or tracker.gg is broken."
-
 
 @client.event
 async def on_ready():
     print('ready-v0.01')
+
+
+"""Server role manager"""
+
+# Verification new users
+VERIFICATION_CHANNEL_ID = [709285744794927125, 819347673575456769]  # Discord TPG (verify-a-friend)
+ROLE_ALLOWED_TO_VERIFY_ID = [
+    818901497244024842,  # Member
+    722195472411787455,  # Coach
+    703688573978804224,  # Staff
+    709595149419675689,  # Director
+    812537908736819280,  # Admin
+    696277516020875324  # Owner
+]
+BOT_ID = 786029312788791346
+VERIFY_ROLE_ID = 703686185968599111
+
+
+@client.command()
+async def verify(ctx):
+    member = ctx.message.author
+    user_roles_list = [role.id for role in member.roles]
+    intersection_roles = set(user_roles_list) & set(ROLE_ALLOWED_TO_VERIFY_ID)
+
+    if len(intersection_roles) > 0:
+        embed = discord.Embed(colour=discord.Colour(0xff001e),
+                              description="✅ User already verified.",
+                              timestamp=datetime.datetime.utcfromtimestamp(1615421803))
+        embed.set_footer(text=f"{member}", icon_url=f"{member.avatar_url}")
+        await ctx.send(embed=embed)
+
+    elif ctx.channel.id in VERIFICATION_CHANNEL_ID:
+        await ctx.message.add_reaction("✅")
+
+
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.channel_id in VERIFICATION_CHANNEL_ID:
+        msg = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
+        member = payload.member
+        roles_user_list = [role.id for role in member.roles]
+        intersection_roles = set(roles_user_list) & set(ROLE_ALLOWED_TO_VERIFY_ID)
+        author = msg.author
+        member = payload.member
+        if int(payload.user_id) != BOT_ID and str(payload.emoji.name) == "✅" and len(intersection_roles) > 0:
+            channel_id = payload.channel_id
+            channel = client.get_channel(channel_id)
+            message = await channel.fetch_message(payload.message_id)
+            role = discord.utils.get(member.guild.roles, id=VERIFY_ROLE_ID)
+
+            await message.delete()
+            await author.add_roles(role)
+
+            success_embed = discord.Embed(colour=discord.Colour(0x8aff02), description="```\n✅ User verified.```")
+
+            success_embed.set_author(name=f"{author}", icon_url=f"{author.avatar_url}")
+            success_embed.set_footer(text=f"{member}", icon_url=f"{member.avatar_url}")
+            await channel.send(embed=success_embed)
+
+            # success_embed = discord.Embed(colour=discord.Colour(0x7ed321),
+            #                               description="✅ User get verified.",
+            #                               timestamp=datetime.datetime.utcfromtimestamp(1615421803))
+            # success_embed.set_footer(text=f"{author}", icon_url=f"{author.avatar_url}")
+            # await channel.send(embed=success_embed)
 
 
 @client.command()
@@ -145,83 +186,84 @@ async def server_info(ctx):
         await ctx.send(embed=embed)
 
 
-@client.event
-async def on_raw_reaction_add(payload):
-    if str(payload.channel_id) in BOT_COMAND_channels_ID:
-        member = payload.member
-        message_id = payload.message_id
-        user_id = payload.user_id
-        channel_id = payload.channel_id
-        msg = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
-        author = msg.author
-        channel = client.get_channel(channel_id)
-        message = await channel.fetch_message(payload.message_id)
-        reaction = get(message.reactions)
-        if str(payload.user_id) != "786029312788791346" and str(payload.emoji.name) == "✅":
-            data_log = {"member_added_reaction": {"member_name": str(member),
-                                                  "author_nick": str(member.nick),
-                                                  "id": str(user_id)},
-                        "message_data": {
-                            "message_id": str(message_id),
-                            "message_author": str(msg.author),
-                            "author_id": str(author.id),
-                            "author_nick": str(author.nick),
-                            "reaction_count": str(reaction.count),
-                            "message_data": str(msg.content),
-                        }
-                        }
-
-            message_author = DiscordUser(member_name=str(member),
-                                         member_id=int(user_id),
-                                         member_nickname=str(member.nick))
-
-            admin_user = DiscordUser(member_name=str(author),
-                                     member_id=int(author.id),
-                                     member_nickname=str(author.nick))
-
-            session = Session()
-            discord_user_create(message_author, session)
-            discord_user_create(admin_user, session)
-
-            # message_author_for_post = session.query(DiscordUser).filter_by(member_id=message_author.member_id).first()
-            # admin_user_for_post = session.query(DiscordUser).filter_by(member_id=admin_user.member_id).first()
-
-            medea_post = MediaPost(message_data=msg.content,
-                                   message_author_id=int(author.id),
-                                   admin_user_id=int(user_id),
-                                   discord_message_id=message_id)
-            print(f"{member.avatar_url}")
-            try:
-                session.add(medea_post)
-                session.commit()
-                print(f"{MediaPost}--CREATE")
-
-                embed = discord.Embed(title=f"Message id - {message_id}",
-                                      colour=discord.Colour(0x18617), url="https://discordapp.com",
-                                      description=f" ```\n{msg.content.replace('!submit', '')}```",
-                                      timestamp=datetime.datetime.utcfromtimestamp(1614909210))
-
-                embed.set_author(name=f"{author}", icon_url=f"{author.avatar_url}")
-                embed.set_footer(text=f"Approved by {member}", icon_url=f"{member.avatar_url}")
-                await channel.send(embed=embed)
-                await message.delete()
-
-            except:
-                print(f"{MediaPost}--ERROR")
-                session.rollback()
-                raise
-            finally:
-                session.close()
-            try:
-                await msg.clear_reaction("❗")
-            except:
-                pass
-        elif str(payload.user_id) != "786029312788791346" and str(payload.emoji.name) == "❌":
-            await message.add_reaction("⭕")
-        elif str(payload.user_id) != "786029312788791346" and str(payload.emoji.name) == "⭕":
-            await message.delete()
-        elif str(payload.user_id) != "786029312788791346" and str(payload.emoji.name) == "❗":
-            await message.add_reaction("✅")
+#
+# @client.event
+# async def on_raw_reaction_add(payload):
+#     if str(payload.channel_id) in BOT_COMAND_channels_ID:
+#         member = payload.member
+#         message_id = payload.message_id
+#         user_id = payload.user_id
+#         channel_id = payload.channel_id
+#         msg = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
+#         author = msg.author
+#         channel = client.get_channel(channel_id)
+#         message = await channel.fetch_message(payload.message_id)
+#         reaction = get(message.reactions)
+#         if str(payload.user_id) != "786029312788791346" and str(payload.emoji.name) == "✅":
+#             data_log = {"member_added_reaction": {"member_name": str(member),
+#                                                   "author_nick": str(member.nick),
+#                                                   "id": str(user_id)},
+#                         "message_data": {
+#                             "message_id": str(message_id),
+#                             "message_author": str(msg.author),
+#                             "author_id": str(author.id),
+#                             "author_nick": str(author.nick),
+#                             "reaction_count": str(reaction.count),
+#                             "message_data": str(msg.content),
+#                         }
+#                         }
+#
+#             message_author = DiscordUser(member_name=str(member),
+#                                          member_id=int(user_id),
+#                                          member_nickname=str(member.nick))
+#
+#             admin_user = DiscordUser(member_name=str(author),
+#                                      member_id=int(author.id),
+#                                      member_nickname=str(author.nick))
+#
+#             session = Session()
+#             discord_user_create(message_author, session)
+#             discord_user_create(admin_user, session)
+#
+#             # message_author_for_post = session.query(DiscordUser).filter_by(member_id=message_author.member_id).first()
+#             # admin_user_for_post = session.query(DiscordUser).filter_by(member_id=admin_user.member_id).first()
+#
+#             medea_post = MediaPost(message_data=msg.content,
+#                                    message_author_id=int(author.id),
+#                                    admin_user_id=int(user_id),
+#                                    discord_message_id=message_id)
+#             print(f"{member.avatar_url}")
+#             try:
+#                 session.add(medea_post)
+#                 session.commit()
+#                 print(f"{MediaPost}--CREATE")
+#
+#                 embed = discord.Embed(title=f"Message id - {message_id}",
+#                                       colour=discord.Colour(0x18617), url="https://discordapp.com",
+#                                       description=f" ```\n{msg.content.replace('!submit', '')}```",
+#                                       timestamp=datetime.datetime.utcfromtimestamp(1614909210))
+#
+#                 embed.set_author(name=f"{author}", icon_url=f"{author.avatar_url}")
+#                 embed.set_footer(text=f"Approved by {member}", icon_url=f"{member.avatar_url}")
+#                 await channel.send(embed=embed)
+#                 await message.delete()
+#
+#             except:
+#                 print(f"{MediaPost}--ERROR")
+#                 session.rollback()
+#                 raise
+#             finally:
+#                 session.close()
+#             try:
+#                 await msg.clear_reaction("❗")
+#             except:
+#                 pass
+#         elif str(payload.user_id) != "786029312788791346" and str(payload.emoji.name) == "❌":
+#             await message.add_reaction("⭕")
+#         elif str(payload.user_id) != "786029312788791346" and str(payload.emoji.name) == "⭕":
+#             await message.delete()
+#         elif str(payload.user_id) != "786029312788791346" and str(payload.emoji.name) == "❗":
+#             await message.add_reaction("✅")
 
 
 @client.command()
@@ -285,4 +327,4 @@ def discord_user_create(checkuser, session):
 
 
 if __name__ == '__main__':
-    client.run(TOKEN)
+    client.run(config.TOKEN)
