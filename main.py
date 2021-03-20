e72e16f0726454f4fd6b17b6f4d600931f328fe1
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2021-03-20 09:06:36 -0700

    APi v0.1.1 - stop

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ tzinfo = datetime.timezone(datetime.timedelta(hours=timezone_offset))
 
 @client.event
 async def on_ready():
-    print('ready-v0.03.11')
+    print('ready-v0.03.12')
 
 
 """auditlog-join-log"""
