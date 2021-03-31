75f910acd32e41f1f5897b097f6c9ef8d81cfe6d
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2021-03-30 20:45:06 -0700

    new Environment settings

--- environment.py
+++ environment.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
-load_dotenv(verbose=True)
 import os
+
+load_dotenv(verbose=True)
 
 
 class _Environment:
@@ -11,13 +12,17 @@ class _Environment:
     API_CLIENT_ID: str
     API_CLIENT_SECRET: str
     PORT: int
+    API_BASE_URL: str
+    DISCORD_BASE_URL: str
 
     def __init__(self):
         self.DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
         self.DISCORD_CLIENT_ID = os.getenv('DISCORD_CLIENT_ID')
         self.DISCORD_CLIENT_SECRET = os.getenv('DISCORD_CLIENT_SECRET')
         self.DISCORD_PUBLIC_KEY = os.getenv('DISCORD_PUBLIC_KEY')
+        self.DISCORD_BASE_URL = os.getenv('DISCORD_BASE_URL')
 
+        self.API_BASE_URL = os.getenv('API_BASE_URL')
         self.API_CLIENT_ID = os.getenv('API_CLIENT_ID')
         self.API_CLIENT_SECRET = os.getenv('API_CLIENT_SECRET')
         self.PORT = int(os.getenv('PORT') or 8082)
@@ -25,7 +30,9 @@ class _Environment:
     def __str__(self):
         return f'DISCORD_BOT_TOKEN={self.DISCORD_BOT_TOKEN}\nDISCORD_CLIENT_ID={self.DISCORD_CLIENT_ID}'
 
+
 __environment = _Environment()
 
-def getEnv():
+
+def get_env():
     return __environment
