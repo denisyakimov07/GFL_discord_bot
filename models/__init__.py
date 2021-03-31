09bafa80aaccb7707c1ce1f3e2616aa99c2a60bc
seismic-cbrooks <cbrooks@seismic.com>
2021-03-31 13:23:41 -0700

    #DBOT-6 Hello world

--- models/__init__.py
+++ models/__init__.py
@@ -108,4 +108,4 @@ class WebhookSubscription(BaseModel):
     model_operations: List[str] = Field(alias='modelOperations')
     model_name: str = Field(alias='modelName')
 
-# This is a test
+# This is a test 2
