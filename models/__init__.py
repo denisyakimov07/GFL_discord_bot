b82c96def3c496d32e1ba0a15beaf0f440f547bb
seismic-cbrooks <cbrooks@seismic.com>
2021-03-31 13:20:53 -0700

    DBOT-6: Hello world

--- models/__init__.py
+++ models/__init__.py
@@ -107,3 +107,5 @@ class WebhookSubscription(BaseModel):
     authorization_header: Optional[str] = Field(alias='authorizationHeader')
     model_operations: List[str] = Field(alias='modelOperations')
     model_name: str = Field(alias='modelName')
+
+# This is a test
