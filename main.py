a547841275245057ac408a702299c084c8ffc0a3
seismic-cbrooks <cbrooks@seismic.com>
2021-04-12 14:37:32 -0700

    Testing something

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from http_server import start_server_thread, app
+from discord_bot import start_discord_bot_thread
+from http_server import start_server_thread
 
 if __name__ == '__main__':
+    start_discord_bot_thread()
     start_server_thread()
