5d03088908e9cdd2cb4b8db9e660dbd7dd3716c3
seismic-cbrooks <cbrooks@seismic.com>
2021-04-12 14:42:35 -0700

    Start http server first

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from discord_bot import start_discord_bot
 from http_server import start_server_thread
 
 if __name__ == '__main__':
-    start_discord_bot()
     start_server_thread()
+    start_discord_bot()
+
